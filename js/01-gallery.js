1f46013d4ff9556a6281c4c644058d0f941a005b
Taras-Repinskyi <repinskiy.taras@gmail.com>
2023-03-09 23:49:05 +0200

    start

---
+++ js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(createGalleryMarkup(items));
+function createGalleryMarkup(items) {
+    const markup = items.map(({preview, oripginal, description}) => {
+        return'
+        <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div >';
+}).join('');
+}
+
+
+
+
+
+console.log(galleryItems);
+    
+
+    
